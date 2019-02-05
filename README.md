046b6e12d15afbb0909406ad524bcb3653f2e5db
Andrew <sokol300@inbox.ru>
2019-02-05 22:28:46 +0400

    fix build

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # presp-installer
 
 ```
-curl -sSL http://89.223.31.207:6000/boot.sh | sudo sh
+curl -sSL http://89.223.31.207:5000/boot.sh | sudo sh
 ```
--- boot.sh
+++ boot.sh
@@ -2,12 +2,12 @@
 echo "Dowload frontend release"
 
 curl http://89.223.31.207:5000/frontend/latest/download > ./frontend.zip
-unzip -f ./frontend.zip -d ./frontend
+unzip ./frontend.zip -d ./frontend
 rm ./frontend.zip
 
 echo "Dowload server release"
 curl http://89.223.31.207:5000/server/latest/download > ./server.zip
-unzip -f ./server.zip -d ./server
+unzip ./server.zip -d ./server
 rm ./server.zip
 
 if hash docker 2>/dev/null; then
