737845bcf05950f3291062373308472166d852a1
Andrew <sokol300@inbox.ru>
2019-02-09 17:13:35 +0400

    native build

--- boot.sh
+++ boot.sh
@@ -13,14 +13,42 @@ curl http://89.223.31.207:5000/server/latest/download > ./server.zip
 unzip ./server.zip -d ./server
 rm ./server.zip
 
-if hash docker 2>/dev/null; then
-    echo "Docker exist"
-else
-    echo "Docker start install"
-    sudo curl -sSL https://get.docker.com | sudo sh
-    sudo apt-get install python-pip -y
-    sudo pip install --upgrade setuptools
-    sudo pip install docker-compose
-fi
+echo "Install mqtt"
+sudo wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
+sudo apt-key add mosquitto-repo.gpg.key
+cd /etc/apt/sources.list.d/
+sudo wget http://repo.mosquitto.org/debian/mosquitto-stretch.list
+sudo apt-get update
+sudo apt-get install mosquitto
+sudo apt-get install mosquitto mosquitto-clients
 
-sudo docker-compose up --build -d
+sudo /etc/init.d/mosquitto stop
+
+sudo cp ./mosquitto.conf /etc/mosquitto/mosquitto.conf
+
+sudo /etc/init.d/mosquitto start
+
+echo "Install mDNS"
+sudo apt-get install avahi-daemon -y
+sudo apt-get install avahi-utils -y
+
+sudo cp ./avahi-daemon.conf /etc/avahi/avahi-daemon.conf
+
+sudo apt-get install libnss-mdns -y
+
+sudo cp ./nsswitch.conf /etc/nsswitch.conf
+
+echo "Install nodejs"
+sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | sudo bash
+
+sudo nvm install 10.15.1
+sudo nvm use 10.15.1
+
+echo "Install pm2"
+npm i -g pm2
+
+pm2 start ./server/main.js --name server
+pm2 save
+pm2 startup
+
+echo "Install success"
